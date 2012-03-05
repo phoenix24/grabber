5e8fd1b86bb3938754ee5710f85ecc111fb5c410
chaitanya sharma <gopi.daiict@gmail.com>
2012-03-05 07:51:35 -0500

    freezing the grabber 0.1 interface.

--- webapp/server.py
+++ webapp/server.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+                           tiles=range(126))
 
 
 if __name__ == '__main__':

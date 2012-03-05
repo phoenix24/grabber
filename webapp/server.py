ac8ce7cc9eb51f942365decccfc8ead2cbf562b8
chaitanya sharma <gopi.daiict@gmail.com>
2012-03-05 10:02:45 -0500

    - added a details/base template page. - added refactored index.html

--- webapp/server.py
+++ webapp/server.py
@@ -11,6 +11,11 @@ def index():
     return render_template("index.html",
                            tiles=range(140))
 
+@app.route("/<elid>")
+def details(elid):
+    return render_template("details.html",
+                           tiles=range(14))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

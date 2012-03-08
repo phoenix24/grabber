195e800d9945b9e21451294a52ce26dd15288479
chaitanya sharma <gopi.daiict@gmail.com>
2012-03-08 09:05:20 -0500

    updated the saholic parser; added scraper for saholic.

--- crawlers/saholic.py
+++ crawlers/saholic.py
@@ -1,9 +1,36 @@
 #!/usr/bash/env python
 
-import os
-import sys
-from httplib2 import Http
-from BeautifulSoup import BeautifulSoup as soup
+from crawler import *
 
+class SaholicInventory(Crawler):
+    """ saholic inventory page. """
 
+    def __init__(self, content, url):
+        super(SaholicInventory, self).__init__(content, url)
+        
+    def get_item_delivery_days(self, item):
+        return 'NA'
+
+    def get_item_price(self, item):
+        return (item.fetch('span', 'newPrice')[1].string).strip()
+
+    def get_item_name(self, item):
+        return (item.fetch('div', 'title')[0].fetch('a')[0].string).strip()
     
+    def list_inventory(self):
+        """ fetch inventory items list. """
+        items = self.pagesp.fetch('div', 'productItem')
+        for index, item in enumerate(items):
+            try:
+                print "%s.  %-25s | %-10s | %s | | %s | %s" % (
+                    index,
+                    self.get_item_name(item),
+                    self.get_item_price(item),
+                    self.get_item_stock_status(item),
+                    self.get_item_delivery_days(item),
+                    #self.get_item_specifications(item),
+                    self.get_item_color(item)
+                    )
+            except:
+                pass
+            
--- crawlers/scraper.py
+++ crawlers/scraper.py
@@ -2,18 +2,22 @@
 
 import logging
 from flipkart import FlipkartInventory
+from saholic import SaholicInventory
 
 
-def scrape_flipkart_mobiles(parser, strtpage = 1, endpage = 3, nextpg = True):
+def scrape_inventory(parser, url, strtpage = 1, endpage = 3):
 
     for page in range(strtpage, endpage):
-        url_to_fetch = "http://flipkart.com/mobiles/all/%s?layout=list" % page
-        
-        # logging.info("scrapper fetching " %  url_to_fetch)
+        url_to_fetch = url % page
         parser('', url_to_fetch).list_inventory()
 
     
 if __name__ == '__main__':
-    scrape_flipkart_mobiles(FlipkartInventory,
-                            strtpage = 1, endpage = 6, nextpg = False)
-    
+
+    # fetch flipkart inventory.
+    scrape_inventory(FlipkartInventory, strtpage = 1, endpage = 2,
+                     url = "http://flipkart.com/mobiles/all/%s?layout=list")
+
+    # fetch saholic inventory.
+    scrape_inventory(SaholicInventory, strtpage = 1, endpage = 2,
+                     url = "http://www.saholic.com/all-mobile-phones/10001?&page=%s")

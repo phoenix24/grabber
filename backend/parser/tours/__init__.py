d0f3a07533482977c07e224f1ff8d531568f7c6d
chaitanya sharma <gopi.daiict@gmail.com>
2012-03-10 06:07:02 -0500

    adding init files.

---
+++ backend/parser/tours/__init__.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+TOURS = {
+    "sotctours" : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.sotctours.com/Destinations.aspx?ServiceType=1&Destination=7"
+            ]
+        },
+    "kesari"   : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.kesari.in/Speciality-Tours/Shimla-Manali-CE.asp"
+            ]
+        },
+    "expedia"   : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.expedia.co.in/holiday-packages.aspx"
+            ]
+        },
+    "crazeal"   : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.crazeal.com/deals/travel"
+            ]
+        },
+    "makemytrip": {
+        "pages" : True,
+        "urls"  : [
+            "http://www.makemytrip.com/holidays-india",
+            "http://www.makemytrip.com/holidays-international"
+            ]
+        },
+    "travelguru": {
+        "pages" : True,
+        "urls"  : [
+            "http://www.travelguru.com/vacations/tours/index.shtml",
+            "http://www.travelguru.com/vacations/International/index.shtml"
+            ]
+        },
+    "snapdeal"  : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.snapdeal.com/getaways-Mumbai"
+            ]
+        },
+    "yatra" : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.yatra.com/Holiday/",
+            "http://www.yatra.com/Holiday/international/",
+            "http://www.yatra.com/hotels-india/fromdelhi.html"
+            ]
+        },
+    "via"   : {
+        "pages" : True,
+        "urls"  : [
+            "http://www.via.com/holidays",
+            "http://www.via.com/holidays/mauritius",
+            "http://www.via.com/holidays/singapore",
+            "http://www.via.com/holidays/dubai",
+            "http://www.via.com/holidays/kerala",
+            "http://www.via.com/holidays/shimla",
+            "http://www.via.com/holidays/manali",
+            "http://www.via.com/holidays/goa"
+            ]
+        }
+    }
+
---
+++ backend/test/suite.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from unittest import main
+
+from test.parser.inventory.test_saholic import *
+from test.parser.inventory.test_flipkart import *
+
+
+if __name__ == '__main__':
+        main()
+
